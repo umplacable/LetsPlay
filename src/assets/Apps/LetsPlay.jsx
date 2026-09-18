1901a5e215b9d3cc8196d29b14c0f4a99f58e290
raphael <raphael.hochlaf@gmail.com>
2026-09-18 11:49:32 +0200

    started LetsPlay

--- src/assets/Apps/LetsPlay.jsx
+++ src/assets/Apps/LetsPlay.jsx
@@ -1,10 +1,24 @@
+import React, { useEffect } from "react";   
 
 const LetsPlay = () => {
-
     
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('/steam_api/ISteamUser/GetPlayerSummaries/v0002/?steamids=76561197960435530,76561198863159519',
+                {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                    },
+                }
+            );
+            console.log(await response.json());
+        }
+        fetchData();
+    },[])
 
     return (
         <div className="letsPlay">
+            <input type="text" placeholder="Friend code, SteamID64, ..." />
 
         </div>
     );
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
 import babel from "@rolldown/plugin-babel";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv  } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig((mode) => {
+	const env = loadEnv(mode, process.cwd(), "")
+	return{
 	plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-});
+	server: {
+		proxy: {
+			'/steam_api': {
+				target: 'https://api.steampowered.com/',
+				changeOrigin: true,
+				rewrite: (path) => {
+					const API_KEY = env.STEAM_API_KEY;
+					path = path.replace(/^\/steam_api/, '')
+					if (path.includes('?')) {
+						path = path.concat(`&key=${API_KEY}`);
+					} else {
+						path = path.concat(`?key=${API_KEY}`);
+					}
+					console.log(path);
+					return path;
+				},
+				cors: false,
+			},
+		},
+	},
+}});
